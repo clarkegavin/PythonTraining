a5da0fcae4e8b7b46887174a6ca81acf2b68d1ed
Gavin Clarke <clarke.gavin@gmail.com>
2024-09-08 16:19:56 +0100

    adding some basic literals

---
+++ literals.py
@@ -0,0 +1,15 @@
+print(type(23))
+print(type(3.14))
+print(type("Hello"))
+
+print(type(None))
+print(type(True))
+print(type(False))
+
+print(type([]))
+print(type({}))
+
+#print(type(hello)) - NameError
+print("Still running")
+
+print(0.1*0.2)
